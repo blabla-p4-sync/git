0c1bca2638e524cc2c0529a0a1202e8edc65d1e4
Brandon Williams <bmwill@google.com>
2017-06-08 16:40:35 -0700

    repository: introduce the repository object
    Introduce the repository object 'struct repository' which can be used to
    hold all state pertaining to a git repository.
    
    Some of the benefits of object-ifying a repository are:
    
      1. Make the code base more readable and easier to reason about.
    
      2. Allow for working on multiple repositories, specifically
         submodules, within the same process.  Currently the process for
         working on a submodule involves setting up an argv_array of options
         for a particular command and then launching a child process to
         execute the command in the context of the submodule.  This is
         clunky and can require lots of little hacks in order to ensure
         correctness.  Ideally it would be nice to simply pass a repository
         and an options struct to a command.
    
      3. Eliminating reliance on global state will make it easier to
         enable the use of threading to improve performance.
    
    Signed-off-by: Brandon Williams <bmwill@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ repository.h
@@ -0,0 +1,25 @@
+#ifndef REPOSITORY_H
+#define REPOSITORY_H
+
+struct repository {
+	/* Environment */
+	char *gitdir;
+	char *commondir;
+	char *objectdir;
+	char *index_file;
+	char *graft_file;
+	char *namespace;
+
+	/* Configurations */
+	unsigned ignore_env:1;
+	/* Indicates if a repository has a different 'commondir' from 'gitdir' */
+	unsigned different_commondir:1;
+};
+
+extern struct repository *the_repository;
+
+extern void repo_set_gitdir(struct repository *repo, const char *path);
+extern int repo_init(struct repository *repo, const char *path);
+extern void repo_clear(struct repository *repo);
+
+#endif /* REPOSITORY_H */
